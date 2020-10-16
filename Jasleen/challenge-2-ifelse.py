db5d80a0142802ad4f2b1e71cb70296ed7e15b81
jasmehra <jasmehra13197@gmail.com>
2020-10-16 10:54:43 +0530

    'challenge-2'

---
+++ Jasleen/challenge-2-ifelse.py
@@ -0,0 +1,14 @@
+# Make an if-else statement where if year is between 2000 and 2099, then print out:
+
+# Welcome to the 21st century!
+
+# Else print out:
+
+# You are before or after the 21st century
+
+year = 2099
+
+if year >= 2000 and year < 2100:
+    print("Welcome to the 21st century!")
+else:
+    print("You are before or after the 21st century") 
