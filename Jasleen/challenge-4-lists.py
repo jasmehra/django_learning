0a0b5a1e1abc1c7a2abe6135178d505e357bd001
jasmehra <jasmehra13197@gmail.com>
2020-10-18 23:53:07 +0530

    lists-challenge

---
+++ Jasleen/challenge-4-lists.py
@@ -0,0 +1,10 @@
+
+# Create a variable named shoes that is a list of the follow items, in order:
+
+shoes = ['Spizikes', 'Air Force 1',  'Curry 2',  'Melo 5']
+
+shoes.insert(4, 'tommy')
+
+del(shoes[4])
+
+print(shoes)
