1d5fd36403de7558716a3a894772eed2c674856a
jasmehra <jasmehra13197@gmail.com>
2020-10-22 23:58:07 +0530

    pasword generator project

--- Jasleen/password_generator_project/generator/views.py
+++ Jasleen/password_generator_project/generator/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    #return "Hii this is my first home page in django!"              #'str' object has no attribute 'get'
+    #return HttpResponse('Hii this is my first home page in django!')
+    return render (request, 'generator/home.html', {'password': 'dkkdkd#djnd@$%jdhjdh'})
+
+def eggs(request):
+    #return HttpResponse('Eggs are very tasty!!!')
+    return HttpResponse('<h1>Eggs are very tasty!!</h1>')       # we can also write html
