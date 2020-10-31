56aa406820044d152b6b10a0308e75cac8431416
tasawwuramd <tasawwuramd@gmail.com>
2020-11-01 00:28:43 +0530

    Completed upto v27

--- T/password_generator-project/generator/views.py
+++ T/password_generator-project/generator/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    # return HttpResponse('Hello there!')
+    return render(request, 'generator/home.html', {'title':'This is Home page'})
+
+
+def password(request):
+    # return HttpResponse('Hello there!')
+    return render(request, 'generator/password.html')
+
+def eggs(request):
+    return HttpResponse('Eggs are so tasty')
