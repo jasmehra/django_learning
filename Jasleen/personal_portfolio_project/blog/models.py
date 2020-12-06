5005d3037008846b3402ffaff86a0ed0a23e7f3b
jasmehra <jasmehra13197@gmail.com>
2020-12-06 18:50:01 +0530

    final commit

--- Jasleen/personal_portfolio_project/blog/models.py
+++ Jasleen/personal_portfolio_project/blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='portfolio/images')
-    url = models.URLField(blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    # image = models.ImageField(upload_to='portfolio/images')
+    # url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+    
