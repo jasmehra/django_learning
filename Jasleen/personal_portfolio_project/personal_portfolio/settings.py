2c25e84bb8373c0aa1e03eb4c92c30062437aadb
jasleen <jasmehra13197@gmail.com>
2020-12-20 13:08:16 +0000

    remove old stuff

--- Jasleen/personal_portfolio_project/personal_portfolio/settings.py
+++ Jasleen/personal_portfolio_project/personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'u0f9pn6j1c3$s9vblm27k$xypcl&(-q3bm+l--%&_kgnz^-s#='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jasleenmehra77.pythonanywhere.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/' 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
- 
