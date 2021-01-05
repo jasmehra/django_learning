58455ccf729d2649cd41d539158b3cbab015ecbd
jasmehra <jasmehra13197@gmail.com>
2021-01-05 10:01:13 +0530

    signup form changes

--- Jasleen/todowoo_project/todo/views.py
+++ Jasleen/todowoo_project/todo/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+        else:
+            print('passwords do not match') 
