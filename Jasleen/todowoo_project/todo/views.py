e0a876caad09125f0ab6b9cf11a3f5c7f3e5e26d
jasmehra <jasmehra13197@gmail.com>
2021-01-17 19:06:12 +0530

    login user form

--- Jasleen/todowoo_project/todo/views.py
+++ Jasleen/todowoo_project/todo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
@@ -25,7 +25,15 @@ def signupuser(request):
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error_message':'Passwords did not match'})
 
 def loginuser(request):
-     return render(request, 'todo/loginuser.html')
+     if request.method == 'GET':
+        return render(request, 'todo/loginuser.html',{'form': AuthenticationForm()})
+     else:
+        user =  authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html',{'form': AuthenticationForm(), 'error_message': 'Username did not match'})
+        else:
+            login(request, user)
+            return redirect('createtodos')
 
 def createtodos(request):
     return render(request, 'todo/createtodos.html')
