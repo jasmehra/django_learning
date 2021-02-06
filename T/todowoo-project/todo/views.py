3009c3ff92ce465af48dce3b1e983240d131b5e1
tasawwuramd <tasawwuramd@gmail.com>
2021-02-06 13:25:38 +0530

    Video 64 Completed

--- T/todowoo-project/todo/views.py
+++ T/todowoo-project/todo/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html', { 'form': UserCreationForm() })
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        # Create new user
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password1'])
+                user.save()
+
+                # login user once they signup
+                login(request, user)
+                return redirect('currenttodos')
+            # for already taken username
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Username already taken'})
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Password Didn\'t Match'})
+            # through password do no match error
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
