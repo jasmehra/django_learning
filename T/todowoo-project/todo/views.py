2215b51e1b8aa10f75ad17c27a3b136809f8a9fd
tasawwuramd <tasawwuramd@gmail.com>
2021-02-06 16:02:10 +0530

    Video 67 completed

--- T/todowoo-project/todo/views.py
+++ T/todowoo-project/todo/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
 
 def signupuser(request):
     if request.method == 'GET':
@@ -24,6 +29,28 @@ def signupuser(request):
         else:
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Password Didn\'t Match'})
             # through password do no match error
+
+
+def logoutuser(request):
+    # Make logout post as chrome and other browser runs all anchor links in background so it might logout in background
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        # Create new user
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
--- T/todowoo-project/todowoo/urls.py
+++ T/todowoo-project/todowoo/urls.py
@@ -21,7 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Auth
     path('signup/', views.signupuser, name="signupuser"),
+    path('login/', views.loginuser, name="loginuser"),
+    path('logout/', views.logoutuser, name="logoutuser"),
 
     # Todos
+    path('', views.home, name="home"),
     path('current/', views.currenttodos, name="currenttodos"),
 ]
